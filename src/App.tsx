3f4bbe8b0c1d86e29947794898c787ea0450342a
Beksultan Chomoev <john.doe@test.com>
2024-01-19 05:32:27 -0600

    Components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import Appbar from "./components/Appbar/Appbar";
+import React from "react";
+
 function App() {
 
-  return (
-    <>
-    </>
-  )
+    return (
+        <React.Fragment>
+            <header>
+                <Appbar />
+            </header>
+            <main className='container-fluid'>
+                Content will be here !
+            </main>
+
+        </React.Fragment>
+    )
 }
 
 export default App
---
+++ src/components/Appbar/Appbar.tsx
@@ -0,0 +1,11 @@
+const Appbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+            <div className="container-fluid">
+                <span className="navbar-brand">User Information</span>
+            </div>
+        </nav>
+    );
+};
+
+export default Appbar;
